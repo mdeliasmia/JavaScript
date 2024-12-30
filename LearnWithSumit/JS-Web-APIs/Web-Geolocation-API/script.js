14ccac117e8a09208fb586da7d1cf765589ced56
Md. Elias Mia <mdeliasmia.aiub@gmail.com>
2024-12-30 11:27:50 +0600

    Create script.js

---
+++ LearnWithSumit/JS-Web-APIs/Web-Geolocation-API/script.js
@@ -0,0 +1,31 @@
+const display = document.querySelector('#display');
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+        //navigator.geolocation.watchPosition(showPosition, showError);
+    } else {
+        display.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    display.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        display.innerHTML = "User denied the request for Geolocation."
+        break;
+      case error.POSITION_UNAVAILABLE:
+        display.innerHTML = "Location information is unavailable."
+        break;
+      case error.TIMEOUT:
+        display.innerHTML = "The request to get user location timed out."
+        break;
+      case error.UNKNOWN_ERROR:
+        display.innerHTML = "An unknown error occurred."
+        break;
+    }
+  }
