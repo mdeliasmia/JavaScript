ba739a8e50e402e2f5451d2398e5b608f0b8b15b
Md. Elias Mia <mdeliasmia.aiub@gmail.com>
2024-12-30 11:25:38 +0600

    Create Web-History-API.js

---
+++ LearnWithSumit/Web-History-API/Web-History-API.js
@@ -0,0 +1,12 @@
+function back(){
+    window.history.back();
+}
+function forward(){
+    window.history.forward();
+}
+function backTo(n){
+    window.history.go(n);
+}
+function getHistory(){
+    alert(window.history.length);
+}
